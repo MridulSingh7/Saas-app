5daa398d72fa093f136b163a83a80e14edf74115
MridulSingh7 <mridulsingh425.personal@gmail.com>
2025-10-04 13:01:48 +0530

    implemented the navbar, make sure to checkout navitems for future references

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/navbar";
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
   subsets: ["latin"],
@@ -18,7 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
+            <Navbar/>
       <body className={`${bricolage.variable} antialiased`}>{children}</body>
     </html>
   );
